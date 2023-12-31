45c112294d6b9186a62cbc5e2e27498cb391e576
Wren Morozova <siliconself@proton.me>
2023-12-30 20:46:47 -0700

    Change to ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 #![doc = include_str!("../README.md")]
 
-use std::{io, thread, time::Duration};
+use std::{io::{self, Stdout}, thread, time::Duration};
 
 use crossterm::{terminal::{enable_raw_mode, EnterAlternateScreen, disable_raw_mode, LeaveAlternateScreen}, execute, event::{EnableMouseCapture, DisableMouseCapture}};
-use tui::{backend::CrosstermBackend, Terminal, widgets::{Block, Borders}};
+use ratatui::{backend::CrosstermBackend, Terminal, widgets::{Block, Borders}};
+
+fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, io::Error> {
+    
+}
 
 fn main() -> Result<(), io::Error> {
     enable_raw_mode()?;
@@ -14,11 +18,11 @@ fn main() -> Result<(), io::Error> {
     terminal.draw(|f| {
         let size = f.size();
         let block = Block::default()
-            .title("Block")
+            .title("Ding Dong Ditch")
             .borders(Borders::ALL);
         f.render_widget(block, size);
     })?;
-    thread::sleep(Duration::from_secs(5));
+    thread::sleep(Duration::from_secs(1));
     disable_raw_mode()?;
     execute!(terminal.backend_mut(), LeaveAlternateScreen, DisableMouseCapture)?;
     terminal.show_cursor()?;
