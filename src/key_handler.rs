9dcdaa25bd8f68a128fe11d0ebfec4d87df5d0dc
Wren Morozova <siliconself@proton.me>
2024-01-02 21:08:39 -0700

    Cleared input when command is entered

--- src/key_handler.rs
+++ src/key_handler.rs
@@ -20,7 +20,9 @@ pub(crate) fn handle_keys() {
                     // Unfortunately we need to clone here to free up the
                     // mutable reference to write_handle. I would like to find a
                     // better way to do this at some point.
-                    let value = write_handle.get_input().value().to_owned();
+                    let input = write_handle.get_input_mut();
+                    let value = input.value().to_owned();
+                    input.reset();
                     write_handle.push_command(&value);
                 }
                 KeyCode::Esc => {
